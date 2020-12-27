b185de85e357a2a2512e43d5af51926c2b297641
DucTai99 <taihandsomeno1@gmail.com>
2020-12-27 23:12:59 +0700

    create translate

--- app/src/main/java/com/example/japan/MainActivity.java
+++ app/src/main/java/com/example/japan/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.japan;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,13 +23,28 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottomNavigationNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-//
         if (savedInstanceState == null) {
             Fragment fragment_Default = new Home();
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment_Default).commit();
         }
     }
-//
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_search){
+            Intent intent = new Intent(MainActivity.this,ContentSearch.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
